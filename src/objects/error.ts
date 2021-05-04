fe1b50337ef5bd906487ba482194d1ffc7f1b729
Quinn T <Quinn.GitHub@gmail.com>
2021-05-04 01:42:08 -0500

    Refactor error.js -> error.ts

--- src/objects/error.ts
+++ src/objects/error.ts
@@ -1,22 +1,24 @@
-// @ts-nocheck
-/* global stolen_sb */
-module.exports = class Error extends global.Error {
+import sbDate from './date';
+
+export default class sbError extends global.Error {
+	parentError: sbError | Error;
+	date: sbDate;
 	/**
 	 * Custom error object - has arguments provided
 	 * @param {Object} obj
 	 * @param {Error} [error]
 	 */
-	constructor (obj, error) {
+	constructor (obj: object, error: Error) {
 		if (!obj || obj.constructor !== Object) {
-			throw new global.Error("stolen_sb.Error must receive an object as params");
+			throw new global.Error("sbError must receive an object as params");
 		}
 
-		const {message, args} = obj;
+		const {message, args} = obj as any;
 		super(message);
 
 		this.parentError = error ?? null;
-		this.name = obj.name || "stolen_sb.Error";
-		this.date = new stolen_sb.Date();
+		this.name = (obj as any).name || "sbError";
+		this.date = new sbDate();
 
 		if (args) {
 			this.message += "; args = " + JSON.stringify(args, null, 2);
@@ -25,9 +27,9 @@ module.exports = class Error extends global.Error {
 		const stackDescriptor = Object.getOwnPropertyDescriptor(this, "stack");
 		Object.defineProperty(this, "stack", {
 			get: () => {
-				const currentStack = (typeof stackDescriptor.get === "function")
-					? stackDescriptor.get()
-					: stackDescriptor.value;
+				const currentStack = (typeof stackDescriptor!.get === "function")
+					? stackDescriptor!.get()
+					: stackDescriptor!.value;
 
 				const extraStack = (this?.parentError?.stack)
 					? `\n=====\nCaused by:\n${this.parentError.stack}`
@@ -39,9 +41,12 @@ module.exports = class Error extends global.Error {
 	}
 
 	toString () {
+		// @ts-ignore
 		let description = super.description;
+		// @ts-ignore
 		if (this.error) {
-			description += `\n${this.err.description}`;
+		// @ts-ignore
+			description += `\n${this.error.description}`;
 		}
 
 		return description;
