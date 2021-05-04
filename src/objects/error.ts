c4f6f95c7dbf8c51a4080e7bc5f8d7108e92819b
Quinn T <Quinn.GitHub@gmail.com>
2021-05-04 02:00:10 -0500

    Fix wrong typing in error.

--- src/objects/error.ts
+++ src/objects/error.ts
@@ -1,14 +1,14 @@
 import sbDate from './date';
 
 export default class sbError extends global.Error {
-	parentError: sbError | Error;
+	parentError: sbError | Error | null;
 	date: sbDate;
 	/**
 	 * Custom error object - has arguments provided
 	 * @param {Object} obj
 	 * @param {Error} [error]
 	 */
-	constructor (obj: object, error: Error) {
+	constructor (obj: object, error?: Error) {
 		if (!obj || obj.constructor !== Object) {
 			throw new global.Error("sbError must receive an object as params");
 		}
