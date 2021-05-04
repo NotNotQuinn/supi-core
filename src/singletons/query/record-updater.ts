d55f6430c23c7a18e794e3d5a72ac7e5346d283a
Quinn T <Quinn.GitHub@gmail.com>
2021-05-03 19:00:56 -0500

    Refactor record-updater.js -> record-updater.ts

--- src/singletons/query/record-updater.ts
+++ src/singletons/query/record-updater.ts
@@ -1,13 +1,14 @@
-// @ts-nocheck
-/* global stolen_sbolen_sb */
+/* global stolen_sb */
+import Query from './index';
+import { WhereHavingParams, FormatSymbol, ColumnDefinition } from './index';
 /**
  * Represents the UPDATE sql statement.
  */
 module.exports = class RecordUpdater {
-	#query = null;
-	#update = { database: null, table: null };
-	#set = [];
-	#where = [];
+	#query: Query|null = null;
+	#update: { database: string|null, table: string|null } = { database: null, table: null };
+	#set: { column: string, value: any }[] = [];
+	#where: string[] = [];
 
 	#priority = "normal";
 	#ignoreDuplicates = false;
@@ -17,14 +18,15 @@ module.exports = class RecordUpdater {
 	 * @param {Query} query
 	 * @name {Recordset}
 	 */
-	constructor (query) {
+	constructor (query: Query) {
 		/** @type {Query} */
-		this.#query = query;
+		this.#query! = query;
 	}
 
-	priority (value) {
+	priority (value: "normal" | "low") {
 		if (!["normal", "low"].includes(value)) {
-			throw new stolen_sbolen_sb.Error({
+			// @ts-ignore
+			throw new stolen_sb.Error({
 				message: "Incorrect priority value",
 				args: { value }
 			});
@@ -45,7 +47,7 @@ module.exports = class RecordUpdater {
 	 * @param {string} table
 	 * @returns {RecordUpdater}
 	 */
-	update (database, table) {
+	update (database:string , table:string ): RecordUpdater {
 		this.#update.database = database;
 		this.#update.table = table;
 		return this;
@@ -57,7 +59,7 @@ module.exports = class RecordUpdater {
 	 * @param {*} value
 	 * @returns {RecordUpdater}
 	 */
-	set (column, value) {
+	set (column: string, value: any): RecordUpdater {
 		this.#set = this.#set.concat({ column, value });
 		return this;
 	}
@@ -69,10 +71,10 @@ module.exports = class RecordUpdater {
 	 * @param {Array.<string|FormatSymbol|WhereHavingParams>} args
 	 * @returns {RecordUpdater}
 	 */
-	where (...args) {
-		let options = {};
+	where (...args: Array<string | FormatSymbol | WhereHavingParams>): RecordUpdater {
+		let options: { condition?: string } = {};
 		if (args[0] && args[0].constructor === Object) {
-			options = args[0];
+			options = args[0] as Object;
 			args.shift();
 		}
 
@@ -82,12 +84,12 @@ module.exports = class RecordUpdater {
 
 		let format = "";
 		if (typeof args[0] === "string") {
-			format = args.shift();
+			format = args.shift() as string;
 		}
 
 		let index = 0;
-		format = format.replace(this.#query.formatSymbolRegex, (fullMatch, param) => (
-			this.#query.parseFormatSymbol(param, args[index++])
+		format = format.replace(this.#query!.formatSymbolRegex, (fullMatch, param) => (
+			this.#query!.parseFormatSymbol(param, args[index++])
 		));
 
 		this.#where = this.#where.concat(format);
@@ -98,24 +100,26 @@ module.exports = class RecordUpdater {
 	/**
 	 * Translates the RecordUpdater to its SQL representation.
 	 * @returns {Promise<string[]>}
-	 * @throws {stolen_sbolen_sb.Error} If no UPDATE database/table have been provided.
-	 * @throws {stolen_sbolen_sb.Error} If no SET columns have been provided.
+	 * @throws {stolen_sb.Error} If no UPDATE database/table have been provided.
+	 * @throws {stolen_sb.Error} If no SET columns have been provided.
 	 */
-	async toSQL () {
+	async toSQL (): Promise<string[]> {
 		if (!this.#update.database || !this.#update.table) {
-			throw new stolen_sbolen_sb.Error({
+			// @ts-ignore
+			throw new stolen_sb.Error({
 				message: "No UPDATE database/table in RecordUpdater - invalid definition"
 			});
 		}
 		else if (this.#set.length === 0) {
-			throw new stolen_sbolen_sb.Error({
+			// @ts-ignore
+			throw new stolen_sb.Error({
 				message: "No SET in RecordUpdater - invalid definition"
 			});
 		}
 
 		const sql = [];
 		const set = [];
-		const { columns } = await this.#query.getDefinition(this.#update.database, this.#update.table);
+		const { columns } = await this.#query!.getDefinition(this.#update.database, this.#update.table) as { columns: ColumnDefinition[] };
 		const priority = (this.#priority === "low") ? "LOW_PRIORITY " : "";
 		const ignore = (this.#ignoreDuplicates) ? "IGNORE " : "";
 
@@ -124,7 +128,8 @@ module.exports = class RecordUpdater {
 		for (const { column, value } of this.#set) {
 			const definition = columns.find(i => i.name === column);
 			if (!definition) {
-				throw new stolen_sbolen_sb.Error({
+			// @ts-ignore
+				throw new stolen_sb.Error({
 					message: `Unrecognized column "${column}"`
 				});
 			}
@@ -133,7 +138,7 @@ module.exports = class RecordUpdater {
 				set.push(`${column} = ${value.value}`);
 			}
 			else {
-				set.push(`${column} = ${this.#query.convertToSQL(value, definition.type)}`);
+				set.push(`${column} = ${this.#query!.convertToSQL(value, definition.type)}`);
 			}
 		}
 
@@ -149,8 +154,8 @@ module.exports = class RecordUpdater {
 	 * Runs the UPDATE SQL query and returns the status object.
 	 * @returns {Object}
 	 */
-	async fetch () {
+	async fetch (): Promise<object> {
 		const sql = await this.toSQL();
-		return await this.#query.raw(...sql);
+		return await this.#query!.raw(...sql);
 	}
 };
